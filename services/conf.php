6f117f5e88152eaa8aaa6eae82e434fd6788aa87
FAOZI <faozimipa@gmail.com>
2017-03-30 10:49:24 +0700

    updeted backup

--- services/conf.php
+++ services/conf.php
@@ -6,17 +6,17 @@ class CONF {
 
 	/* Data configuration for database */
 	public $DB_SERVER = "localhost";
-	public $DB_USER = "root";
-	public $DB_PASSWORD = "";
-	public $DB_NAME = "admin_tumbas";
+	public $DB_USER = "tumbas_admin";
+	public $DB_PASSWORD = "9undulP4cul";
+	public $DB_NAME = "tumbas_admin";
 	
 	/* FCM key for notification */
-	public $FCM_KEY = "AIzaSyCv-90mFpx3SCWlIKSXXXXXXXXXXXXXXXXXXXXXX";
+	public $FCM_KEY = "AAAAsOM5kL8:APA91bFmeNbRrBTaXPTDQqN720vYAC9wPiTC_txiVNI-WRwmrKC7h6o1pUCzhoyG_6YaPAGOxw3gWilsMTQxE_HMjB5CS9fUVVXBhh_zhNwHbEn-DXuYGnfSWSb5rEyktMzTlECF-hie";
 
 
     /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
     /* This string must be same with security code at Android, if its different android unable to submit order */
-	public $SECURITY_CODE = "8V06LupAaMBLtQqyqTxmcN42nn27FlejvaoSM3zXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	public $SECURITY_CODE = "jHJfSYHGWWeJWLZzVPag35PW8Fu8sySq69b2Oks9sNeLuCIUAIF3HdnJHjhvTpymhv3UMAlpo2ZkAW8YMyDGwEf56EInw0aaIbJ1";
 	
 }	
 ?>
